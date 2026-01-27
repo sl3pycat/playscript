1937a7539dc9c190fdff6cda7320f8077cfb5c63
sl3pycat <sl3pycat@gmail.com>
2026-01-27 23:56:20 +0200

    import overhaul

--- api/import.js
+++ api/import.js
@@ -1,6 +1,17 @@
 psjs.define("import",(target, value, extension, item)=>{
-  //handles playsript json
-  if(extension === undefined)psjs.fetch(value).then(response=>{
-    try{psjs.parse(JSON.parse(response),target)}catch(json_parsing_error){/*TODO: handle ts*/}
+  
+  psjs.fetch(value).then(response=>{
+    
+    //handles playsript json
+    if(extension === undefined)try{psjs.parse(JSON.parse(response),target)}catch(json_parsing_error){/*TODO: handle ts*/}
+    
+    //handles html
+    if(extension === "html")target.insertAdjacentHTML("beforeend", response)
+    
+    //handles css
+    if(extension === "css")target.insertAdjacentHTML("beforeend", `<style>${response}</style>`)
+    
+    //handles js
+    if(extension === "js")try{Function(response)()}catch(json_parsing_error){/*TODO: handle ts*/}
   })
 },"primary")
--- api/import.md
+++ api/import.md
@@ -1,13 +1,9 @@
 API import
 ---
-Imports playscript code and executes it on the target node.
-
-```json
-{
-  "import":"/path/to/external_code.json"
-}
-```
-
-**note** imports are async and in race mode, the script that loads first is executed first.
-
----
+### Cheatsheet
+Method | Accepts | Description 
+--- | --- | ---
+import | String | Imports playscript json to be executed at the target level.
+import.html | String | Imports to be injected to the target.
+import.js | String | Imports javascript to be executed in a scoped function.
+import.css | String | Imports styles at the tatget level, styles globally.
